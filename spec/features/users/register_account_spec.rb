09d75da79607234fe4cf908da3f248e63f308297
Mike <mikecm@gmail.com>
2019-01-05 16:01:34 -0700

    Add sad path testing if passwords do not match

--- spec/features/users/register_account_spec.rb
+++ spec/features/users/register_account_spec.rb
@@ -13,13 +13,27 @@ describe 'Users' do
       fill_in :user_last_name, with: 'McKee'
       fill_in :user_email, with: 'mikecm@gmail.com'
       fill_in :user_password, with: 'password'
-      fill_in :user_password_confirmation, with: 'password_confirmation'
+      fill_in :user_password_confirmation, with: 'password'
       click_on 'Create Account'
 
       expect(current_path).to eq dashboard_path
       expect(page).to have_content 'Welcome Mike McKee'
       expect(page).to have_content 'Log Out'
       expect(page).to_not have_content 'Sign Up'
+    end
+
+    it 'denies account creation if passwords do not match' do
+      visit register_path
+
+      fill_in :user_first_name, with: 'Mike'
+      fill_in :user_last_name, with: 'McKee'
+      fill_in :user_email, with: 'mikecm@gmail.com'
+      fill_in :user_password, with: 'password'
+      fill_in :user_password_confirmation, with: 'password_no_match'
+      click_on 'Create Account'
+
+      expect(current_path).to eq register_path
+      expect(page).to have_content 'Password confirmation doesn\'t match Password'
     end
   end
 end
