2a8bdcbb721a62f50e00f4704c8d7f6e0283d908
Mike <mikecm@gmail.com>
2019-01-05 16:00:52 -0700

    Add branch logic for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,31 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id.to_s
+      redirect_to dashboard_path
+    else
+      flash.now[:notice] = @user.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(current_user.id)
+  end
+
+  private
+
+  def user_params
+    params
+      .require(:user)
+      .permit(:first_name,
+              :last_name,
+              :email,
+              :password,
+              :password_confirmation
+            )
+  end
 end
