0a40b816e40c3b9696827361e45fdcc66f60cee7
Mike <mikecm@gmail.com>
2019-01-05 15:17:31 -0700

    Update alignment on routes page for readability

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   # User Routes
-  get '/register', to: 'users#new', as: 'register'
+  get '/register',  to: 'users#new',    as: 'register'
   post '/register', to: 'users#create', as: 'users'
-  get '/dashboard', to: 'users#show', as: 'dashboard'
+  get '/dashboard', to: 'users#show',   as: 'dashboard'
 end
