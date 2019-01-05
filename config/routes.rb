49f6dcf1c7bd0095b435be956ef598079127f900
Mike <mikecm@gmail.com>
2019-01-05 16:42:28 -0700

    Add login path routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  # Session Routes
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create', as: 'new_login'
+
   # User Routes
   get '/register',  to: 'users#new',    as: 'register'
   post '/register', to: 'users#create', as: 'users'
