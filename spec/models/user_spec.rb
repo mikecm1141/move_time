10e32aac73c1c51896d7ebe73aaa0759ac1637df
Mike <mikecm@gmail.com>
2019-01-05 15:20:34 -0700

    Create User model validations spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User, type: :model do
+  context 'Validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :password }
+    it { should validate_presence_of :password_confirmation }
+  end
 end
