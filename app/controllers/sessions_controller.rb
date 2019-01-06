938c7ff5fe6f3e59dcef1cfa8f97045888cde1a0
Mike <mikecm@gmail.com>
2019-01-05 17:32:58 -0700

    Refactor Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,14 @@
 
 # Class for handling sessions at the controller level
 class SessionsController < ApplicationController
-  before_action :user, only: :create
+  before_action :user, :authenticate_user, :validate_auth, only: :create
   before_action :require_user, :destroy_session, only: :destroy
 
   def new; end
 
   def create
-    if user && user.authenticate(session_params[:password])
-      session[:user_id] = user.id.to_s
-      flash[:success] = 'Successfully signed in'
-      redirect_to dashboard_path
-    else
-      flash.now[:error] = 'Invalid email or password'
-      render :new
-    end
+    session[:user_id], flash[:success] = user.id.to_s, 'Successfully signed in'
+    redirect_to dashboard_path
   end
 
   def destroy
@@ -31,6 +25,17 @@ class SessionsController < ApplicationController
 
   def user
     @user ||= User.find_by(email: session_params[:email])
+  end
+
+  def authenticate_user
+    user&.authenticate(session_params[:password])
+  end
+
+  def validate_auth
+    unless authenticate_user
+      flash.now[:error] = 'Invalid email or password'
+      render :new
+    end
   end
 
   def destroy_session
