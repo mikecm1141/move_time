c25bf3a47761d4571d9d06fb60f137dd5802662a
Mike <mikecm@gmail.com>
2019-01-05 18:45:45 -0700

    Create search route

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   # Session Routes
-  get '/login', to: 'sessions#new', as: 'login'
-  post '/login', to: 'sessions#create', as: 'new_login'
+  get '/login',     to: 'sessions#new',     as: 'login'
+  post '/login',    to: 'sessions#create',  as: 'new_login'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
 
   # User Routes
   get '/register',  to: 'users#new',    as: 'register'
   post '/register', to: 'users#create', as: 'users'
   get '/dashboard', to: 'users#show',   as: 'dashboard'
+
+  # Search Routes
+  get '/search', to: 'search#index'
 end
