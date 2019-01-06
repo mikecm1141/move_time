e8675dcb42ea1dd22a46db02d2653e40c38229cd
Mike <mikecm@gmail.com>
2019-01-05 19:37:08 -0700

    Create spec and facade for city search results

---
+++ app/facades/city_search_result.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Facade for the city search results
+class CitySearchResult
+  attr_reader :query
+
+  def initialize(query)
+    @query = query
+  end
+
+  def cities
+    city_results[:'city:search-results'].map do |city_data|
+      CitySearchItem.new(city_data)
+    end
+  end
+
+  def count
+    cities.count
+  end
+
+  private
+
+  attr_reader :city_results
+
+  def teleport_service
+    TeleportService.new
+  end
+
+  def city_results
+    @city_results ||= teleport_service.city_results(query)[:_embedded]
+  end
+end
