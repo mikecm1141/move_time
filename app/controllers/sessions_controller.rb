6269478dada04775b38ad8581b7b6ab745f9aeb2
Mike <mikecm@gmail.com>
2019-01-05 17:16:15 -0700

    Add destroy action for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,24 @@
 # Class for handling sessions at the controller level
 class SessionsController < ApplicationController
   before_action :get_user, only: :create
+  before_action :require_user, :destroy_session, only: :destroy
 
   def new; end
 
   def create
     if user && user.authenticate(session_params[:password])
       session[:user_id] = user.id.to_s
-      flash[:success] = 'Successfully logged in'
+      flash[:success] = 'Successfully signed in'
       redirect_to dashboard_path
     else
-      flash.now[:notice] = 'Invalid email or password'
+      flash.now[:error] = 'Invalid email or password'
       render :new
     end
+  end
+
+  def destroy
+    flash[:success] = 'Successfully signed out'
+    redirect_to root_path
   end
 
   private
@@ -26,5 +32,9 @@ class SessionsController < ApplicationController
 
   def get_user
     @user ||= User.find_by(email: session_params[:email])
+  end
+
+  def destroy_session
+    session.delete(:user_id)
   end
 end
