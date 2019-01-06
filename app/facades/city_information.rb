ac9d744e360eeb7122e2cc9b753e7f7c806e526d
Mike <mikecm@gmail.com>
2019-01-06 00:10:01 -0700

    Add some basic data to city show page

--- app/facades/city_information.rb
+++ app/facades/city_information.rb
@@ -14,8 +14,36 @@ class CityInformation
     city_information[:display_name]
   end
 
+  def info_year
+    city_data[:year]
+  end
+
   def population
     city_data[:pop]
+  end
+
+  def median_age
+    city_data[:age]
+  end
+
+  def mean_commute_minutes
+    city_data[:mean_commute_minutes]&.ceil || 'N/A'
+  end
+
+  def median_household_income
+    city_data[:income]
+  end
+
+  def median_property_value
+    city_data[:median_property_value]
+  end
+
+  def pop_rank
+    city_data[:pop_rank].to_i
+  end
+
+  def income_rank
+    city_data[:income_rank].to_i
   end
 
   private
@@ -26,5 +54,13 @@ class CityInformation
 
   def city_data
     @city_data ||= hashed_data(data_usa_service.city_data(id))[0]
+  end
+
+  def transport_data
+    @transport_data ||= hashed_data(data_usa_service.transport_data(id))[0]
+  end
+
+  def ethnicity_data
+    @ethnicity_data ||= hashed_data(data_usa_service.ethnicity_data(id))[0]
   end
 end
