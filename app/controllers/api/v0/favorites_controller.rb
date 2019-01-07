681d656e341a8c14353ebb4d3edf35ecd31e519e
Mike McKee <mikecm@gmail.com>
2019-01-07 14:07:43 -0700

    Fix HoundCI linter issues

--- app/controllers/api/v0/favorites_controller.rb
+++ app/controllers/api/v0/favorites_controller.rb
@@ -3,17 +3,17 @@ class Api::V0::FavoritesController < ApplicationController
   before_action :user, only: %i[create destroy]
 
   def create
-    user.favorites
-      .unscoped
-      .find_or_create_by(user: user, geo_id: params[:geo_id])
-      .update(active: true)
+    user.favorites.unscoped
+        .find_or_create_by(user: user, geo_id: params[:geo_id])
+        .update(active: true)
+
     render json: { 'message': 'Favorite added' }, status: 201
   end
 
   def destroy
     user.favorites
-      .find_by(geo_id: params[:geo_id])
-      .update(active: false)
+        .find_by(geo_id: params[:geo_id])
+        .update(active: false)
 
     render json: { 'message': 'Favorite removed' }, status: 204
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 User.create!(
   first_name:            'Mike',
   last_name:             'McKee',
   email:                 'mikecm@gmail.com',
   password:              'pass',
-  password_confirmation: 'pass')
+  password_confirmation: 'pass'
+  )
