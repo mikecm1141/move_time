a2ce354d3436000fd5a5212eb879fca6fdbd3e23
Mike McKee <mikecm@gmail.com>
2019-01-07 14:01:01 -0700

    Refactor API controller for one-to-many table update

--- app/controllers/api/v0/favorites_controller.rb
+++ app/controllers/api/v0/favorites_controller.rb
@@ -1,30 +1,26 @@
 class Api::V0::FavoritesController < ApplicationController
   skip_before_action :verify_authenticity_token 
-  before_action :user, :favorite, only: %i[create destroy]
+  before_action :user, only: %i[create destroy]
 
   def create
-    if user.set_favorite(favorite)
-      render json: { 'message': 'Favorite added' }, status: 201
-    else
-      render json: { 'message': 'Something went wrong!' }, status: 400
-    end
+    user.favorites
+      .unscoped
+      .find_or_create_by(user: user, geo_id: params[:geo_id])
+      .update(active: true)
+    render json: { 'message': 'Favorite added' }, status: 201
   end
 
   def destroy
-    if user.deactivate_favorite(favorite)
-      render json: { 'message': 'Favorite removed'}, status: 204
-    else
-      render json: { 'message': 'Something went wrong!' }, status: 400
-    end
+    user.favorites
+      .find_by(geo_id: params[:geo_id])
+      .update(active: false)
+
+    render json: { 'message': 'Favorite removed' }, status: 204
   end
 
   private
 
   def user
     @user ||= User.find_by_id(params[:user_id])
-  end
-
-  def favorite
-    @favorite ||= Favorite.find_or_create_by(geo_id: params[:geo_id])
   end
 end
