b7c13bf8725232b946b874ecf4cad77dc9f1380c
Mike <mikecm@gmail.com>
2019-01-08 12:17:33 -0700

    Add save_view method to save a view when a user looks at a city

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -2,7 +2,15 @@
 
 # Controller for City show
 class CityController < ApplicationController
+  after_action :save_view, only: :show
+
   def show
     @city = CityInformation.new(params[:id])
+  end
+
+  private
+
+  def save_view
+    current_user.views.find_or_create_by(geo_id: params[:id]) if current_user
   end
 end
