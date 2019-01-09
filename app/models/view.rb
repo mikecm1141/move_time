dec3038bd7ce8b2f3c1dd366a90243068f0de24a
Mike <mikecm@gmail.com>
2019-01-08 18:34:16 -0700

    Factor out common methods into module

--- app/models/view.rb
+++ app/models/view.rb
@@ -1,3 +1,13 @@
 class View < ApplicationRecord
+  include Informational
+
   belongs_to :user
+
+  def name
+    external_data[:display_name]
+  end
+
+  def slug
+    external_data[:url_name]
+  end
 end
